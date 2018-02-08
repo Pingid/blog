54ae29c97c5f49f92cf165d5e9d6d48a4452f340
Dan <dan@Macs-MacBook-Pro.local>
2018-02-08 00:34:24 +0000

    essay change

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -2,31 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 import ReactImageGallery from 'react-image-gallery';
 
+const Image = styled.img`
+	width: auto !important;
+`;
+
+const NavIcon = styled.div`
+	padding: .3rem;
+  background: #f7f7f75;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10;
+`;
+
+const NavIconImage = styled.img`
+  width: 1.2rem;
+  cursor: pointer;
+  transition: .3s transform;
+  opacity: ${({ disabled }) => disabled ? 0 : .8 };
+  :hover {
+    transform: scale(1.1,1.1)
+  }
+`
+
 const ImageGallery = ({ items, style }) => {
 	const Item = styled.div`
 		height: ${style.height};
 		width: ${style.width};
 		background-color: #f7f7f7;
-	`
-	const Image = styled.img`
-		width: auto !important;
-	`
-	const NavIcon = styled.div`
-		padding: .3rem;
-    background: #f7f7f75;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    z-index: 10;
-  `
-	const NavIconImage = styled.img`
-	  width: 1.2rem;
-	  cursor: pointer;
-	  transition: .3s transform;
-	  opacity: ${({ disabled }) => disabled ? 0 : .8 };
-	  :hover {
-	    transform: scale(1.1,1.1)
-	  }
 	`
 	return (
 	  <ReactImageGallery
